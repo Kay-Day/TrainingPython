209c56da92d836b5eb13868821fbfefd6cc938d2
Kay-Day <caoman26@gmail.com>
2024-01-14 19:07:38 +0700

    Upload

---
+++ LapCode/Dictionary.py
@@ -0,0 +1,13 @@
+newDictionary = {
+    "id": 123,
+    "name": "Nghia",
+    "Gender": True
+}
+print(newDictionary)
+print(newDictionary.get("id"))
+print(type(newDictionary))
+print(newDictionary["name"])
+x = newDictionary.keys()
+print(x)
+print("id" in newDictionary)
+print("gmail" in newDictionary)
